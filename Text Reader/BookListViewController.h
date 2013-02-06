387c5544bca2e3a0f222e177564196a813d07c38
Satyam Ghodasara <Satyam@Satyam-Ghodasaras-MacBook-Pro.local>
2013-02-06 12:00:10 -0500

    More renaming

--- Text Reader/BookListViewController.h
+++ Text Reader/BookListViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "PageViewController.h"
 
-@interface PageListViewController : UITableViewController
+@interface BookListViewController : UITableViewController
 
 @property NSMutableDictionary *images;
 @property NSString *mainPath;
