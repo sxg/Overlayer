0af28c0870bcfee26c628a26effff376932e9092
Satyam Ghodasara <sghodas@gmail.com>
2013-09-12 00:01:13 -0400

    Table view supports adding books

---
+++ Text Reader/SGBookListViewController.m
@@ -0,0 +1,106 @@
+//
+//  SGBookListViewController.m
+//  Text Reader
+//
+//  Created by Satyam Ghodasara on 9/11/13.
+//  Copyright (c) 2013 Satyam Ghodasara. All rights reserved.
+//
+
+#import "SGBookListViewController.h"
+#import "SGBook.h"
+
+@interface SGBookListViewController ()
+
+@property (nonatomic, readwrite, strong) NSMutableArray *books;
+
+@end
+
+@implementation SGBookListViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    _books = [[NSMutableArray alloc] init];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - SGAddBook delegate
+
+- (void)addBookController:(SGAddBookController *)addBookVC didAddBookWithTitle:(NSString *)title
+{
+    SGBook *book = [[SGBook alloc] initWithTitle:title];
+    [_books addObject:book];
+    
+    [self.tableView reloadData];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return _books.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"bookCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    cell.textLabel.text = [[_books objectAtIndex:indexPath.row] title];
+    
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Navigation logic may go here. Create and push another view controller.
+    /*
+     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
+     // ...
+     // Pass the selected object to the new view controller.
+     [self.navigationController pushViewController:detailViewController animated:YES];
+     */
+}
+
+#pragma mark - Segue
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([segue.identifier isEqualToString:@"addBook"]) {
+        SGAddBookController *addBookVC = (SGAddBookController *)[[segue.destinationViewController viewControllers] lastObject];
+        [addBookVC setDelegate:self];
+    }
+}
+
+@end
