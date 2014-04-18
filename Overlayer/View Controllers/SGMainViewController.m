804163db5408810abc4bb77a060f933189295c89
Satyam Ghodasara <sghodas@gmail.com>
2014-04-18 00:29:24 -0400

    Replaced UIView animation method that didn't exist in iOS 6 and fixed layout issues with iOS 6

--- Overlayer/View Controllers/SGMainViewController.m
+++ Overlayer/View Controllers/SGMainViewController.m
@@ -58,37 +58,38 @@
 {
     if (self.isDisplayingSidePane) {
         __block SGMainViewController *blockSelf = self;
-        [UIView animateWithDuration:1.0 delay:0 usingSpringWithDamping:1.0f initialSpringVelocity:0.0f options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        [UIView animateWithDuration:1.0 delay:0 options:UIViewAnimationOptionCurveEaseInOut animations:^{
             
+            //  Push the side pane view up so that only the bottom 42pts can be seen (this is where the toggle button is)
             CGRect endFrame = CGRectMake(CGRectGetMinX(blockSelf.sidePaneView.frame),
-                                         (-1*CGRectGetHeight(blockSelf.sidePaneView.frame))+30,
+                                         (-1*CGRectGetHeight(blockSelf.sidePaneView.frame))+42,
                                          CGRectGetWidth(blockSelf.sidePaneView.frame),
                                          CGRectGetHeight(blockSelf.sidePaneView.frame));
             blockSelf.sidePaneView.frame = endFrame;
             
+            //  Rotate the toggle side pane button 180º
             blockSelf.toggleSidePaneView.transform = CGAffineTransformMakeRotation(M_PI);
             
         } completion:^(BOOL finished) {
-            
+            blockSelf.displayingSidePane = NO;
         }];
-        
-        self.displayingSidePane = NO;
     } else {
         __block SGMainViewController *blockSelf = self;
-        [UIView animateWithDuration:1.0 delay:0 usingSpringWithDamping:1.0f initialSpringVelocity:1.0f options:UIViewAnimationOptionCurveEaseInOut animations:^{
+        [UIView animateWithDuration:1.0 delay:0 options:1.0f animations:^{
             
+            //  Push the side pane view back down to its original position
             CGRect endFrame = CGRectMake(CGRectGetMinX(blockSelf.sidePaneView.frame),
                                          CGRectGetMinY(blockSelf.view.frame),
                                          CGRectGetWidth(blockSelf.sidePaneView.frame),
                                          CGRectGetHeight(blockSelf.sidePaneView.frame));
             blockSelf.sidePaneView.frame = endFrame;
-
+            
+            //  Rotate the toggle side pane button 180º
             blockSelf.toggleSidePaneView.transform = CGAffineTransformMakeRotation(0);
             
         } completion:^(BOOL finished) {
-            
+            blockSelf.displayingSidePane = YES;
         }];
-        self.displayingSidePane = YES;
     }
 }
 
