47909f040d4789e6d9edcefc1ed24667d55c7abf
Satyam Ghodasara <sghodas@gmail.com>
2014-04-18 00:01:13 -0400

    Toggle side pane button now rotates back and forth when toggled

--- Overlayer/View Controllers/SGMainViewController.m
+++ Overlayer/View Controllers/SGMainViewController.m
@@ -23,6 +23,7 @@
 @property (readwrite, weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @property (readwrite, weak, nonatomic) IBOutlet UIView *sidePaneView;
+@property (readwrite, weak, nonatomic) IBOutlet UIButton *toggleSidePaneView;
 
 @property (readwrite, assign, getter = isDisplayingSidePane) BOOL displayingSidePane;
 
@@ -56,26 +57,33 @@
 - (IBAction)didTapToggleSidePaneButton:(UIButton *)sender
 {
     if (self.isDisplayingSidePane) {
+        __block SGMainViewController *blockSelf = self;
         [UIView animateWithDuration:1.0 delay:0 usingSpringWithDamping:1.0f initialSpringVelocity:0.0f options:UIViewAnimationOptionCurveEaseInOut animations:^{
             
-            CGRect endFrame = CGRectMake(CGRectGetMinX(self.sidePaneView.frame),
-                                         (-1*CGRectGetHeight(self.sidePaneView.frame))+30,
-                                         CGRectGetWidth(self.sidePaneView.frame),
-                                         CGRectGetHeight(self.sidePaneView.frame));
-            self.sidePaneView.frame = endFrame;
+            CGRect endFrame = CGRectMake(CGRectGetMinX(blockSelf.sidePaneView.frame),
+                                         (-1*CGRectGetHeight(blockSelf.sidePaneView.frame))+30,
+                                         CGRectGetWidth(blockSelf.sidePaneView.frame),
+                                         CGRectGetHeight(blockSelf.sidePaneView.frame));
+            blockSelf.sidePaneView.frame = endFrame;
+            
+            blockSelf.toggleSidePaneView.transform = CGAffineTransformMakeRotation(M_PI);
             
         } completion:^(BOOL finished) {
             
         }];
+        
         self.displayingSidePane = NO;
     } else {
+        __block SGMainViewController *blockSelf = self;
         [UIView animateWithDuration:1.0 delay:0 usingSpringWithDamping:1.0f initialSpringVelocity:1.0f options:UIViewAnimationOptionCurveEaseInOut animations:^{
             
-            CGRect endFrame = CGRectMake(CGRectGetMinX(self.sidePaneView.frame),
-                                         CGRectGetMinY(self.view.frame),
-                                         CGRectGetWidth(self.sidePaneView.frame),
-                                         CGRectGetHeight(self.sidePaneView.frame));
-            self.sidePaneView.frame = endFrame;
+            CGRect endFrame = CGRectMake(CGRectGetMinX(blockSelf.sidePaneView.frame),
+                                         CGRectGetMinY(blockSelf.view.frame),
+                                         CGRectGetWidth(blockSelf.sidePaneView.frame),
+                                         CGRectGetHeight(blockSelf.sidePaneView.frame));
+            blockSelf.sidePaneView.frame = endFrame;
+
+            blockSelf.toggleSidePaneView.transform = CGAffineTransformMakeRotation(0);
             
         } completion:^(BOOL finished) {
             
